a05010e9d94bc36487a449b63f6110246bd54ede
Colin Stanford <j.colin.stanford@gmail.com>
2024-06-15 13:15:51 -0600

    first commit

---
+++ frannie.js
@@ -0,0 +1,13 @@
+/*
+Your users should be able to:
+
+Hide/Show the answer to a question when the question is clicked
+Navigate the questions and hide/show answers using keyboard navigation alone
+View the optimal layout for the interface depending on their device's screen size
+See hover and focus states for all interactive elements on the page*/
+
+document.querySelector('span').addEventListener("click", expand);
+
+function expand() {
+  document.querySelector('p').classList.toggle('p');
+}
